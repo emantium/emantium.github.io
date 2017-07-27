71b216c6d20d72809c3b25206e821795c9ddfe43
vzkiss <vzkiss@gmail.com>
2017-07-27 00:12:18 -0700

    phpmailer

--- assets/js/contact_mailer.js
+++ assets/js/contact_mailer.js
@@ -21,27 +21,26 @@
 				var email           = $("input#cemail").val();
 				var message         = $("textarea#cmessage").val();
 
+				var sendData = $('#contact-form').serialize();
+
 				$.ajax({
 					type: 'POST',
-					url: 'assets/php/contact.php',
-					dataType: 'json',
-					data: {
-						name: name,
-						email: email,
-						message: message,
-					},
-					cache: false,
+					url: 'assets/php/contact_mailer.php',
+					//dataType: 'json',
+					data: sendData,
+					//cache: false,
 					beforeSend: function(result) {
 						submit.empty();
 						submit.append('<i class="fa fa-cog fa-spin"></i> Wait...');
 					},
 					success: function(result) {
-						if(result.sendstatus == 1) {
-							ajaxResponse.html(result.message);
+
+						if(result === 'success') {
+							ajaxResponse.html("Thank you for your email! I'll be in touch shortly.");
 							$form.fadeOut(500);
 							console.log(result);
 						} else {
-							ajaxResponse.html(result.message);
+							ajaxResponse.html(result);
 						}
 					}
 				});
---
+++ assets/php/contact_mailer.php
@@ -0,0 +1,72 @@
+<?php
+
+	// Mail settings
+	$to      = "vzkiss@gmail.com";
+	$subject = "Emantium Inquiry";
+
+
+	if (isset($_POST["cname"]) && isset($_POST["cemail"]) && isset($_POST["cmessage"])) {
+
+		$content  = "Name: "     . $_POST["cname"]    . "\r\n";
+		$content .= "Email: "    . $_POST["cemail"]   . "\r\n";
+		$content .= "Message: "  . "\r\n" . $_POST["cmessage"];
+
+
+		$name = $_POST["cname"];
+		$email = $_POST["cemail"];
+
+		$errorMsg = "";
+
+		require 'phpmailer/PHPMailerAutoload.php';
+	
+		// new PHPMailer 
+		$mail = new PHPMailer();
+
+		// PHPMailer ValidateAddress 
+		if( !$mail->ValidateAddress($email) ){
+			$errorMsg = "Email address is invalid.";
+		}
+
+		//Email setup and send
+		$mail->setFrom("info@test.com", "your name");
+		$mail->AddReplyTo($email, $name);
+		$mail->addAddress($to, 'VZKiss.com');
+
+
+		$mail->isHTML(true);	// Set email format to HTML
+
+		$mail->Subject =  $subject . ' - Contact Form Submission';
+
+		$email_body = "";
+		$email_body = "You have got the following message:<br />";
+		$email_body = $email_body . "--------------------<br /><br />" . $content;
+
+		$mail->Body = $email_body;
+
+		// Send email
+		if (!$mail->send()) {
+			if ($errorMsg == "") {
+		    	echo "There was a problem sending the email: " . $mail->ErrorInfo;
+		    	redirect_no_ajax();
+		    	exit;
+		    } else {
+		    	echo $errorMsg;
+		    	redirect_no_ajax();
+		    	exit;
+		    }
+		}
+
+		// Redirect after successful email
+		echo "success";
+		exit;
+	}
+
+/* AJAX check  */
+function redirect_no_ajax() {
+	
+	if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+		/* if not ajax redirect */
+		header("Location: index.php#contact");
+		exit;
+	}
+}
